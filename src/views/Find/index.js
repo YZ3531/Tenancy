458862ae1ed725ed474cf697aaee309fa0848c40
YZ3531 <2418998035@qq.com>
2020-01-17 21:31:31 +0800

    找房模块基本布局

--- src/views/Find/index.js
+++ src/views/Find/index.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import { Flex } from 'antd-mobile'
+import './index.scss'
+import { getCurrentCity } from '../../utils/API'
+// 引入组件
+import Filter from './components/Filter'
 
-class HomeFind extends React.Component{
-    render(){
-        return(
-            <div>找房</div>
+class Find extends React.Component {
+    state = {
+        currentCity:""
+    }
+    async componentDidMount(){
+        let res = await getCurrentCity()
+        this.setState({
+            currentCity:res.label
+        })
+    }
+    render () {
+        let {currentCity} = this.state
+        return (
+            <div className='find'>
+                {/* 顶部导航栏 */}
+                <Flex className='header'>
+                    <i className="iconfont icon-back" />
+                    <Flex className='search-box searchHeader'>
+                        {/* 左侧白色区域 */}
+                        <Flex className="search">
+                            {/* 位置 */}
+                            <div className="location" >
+                                <span className="name">{currentCity}</span>
+                                <i className="iconfont icon-arrow" />
+                            </div>
+                            {/* 搜索表单 */}
+                            <div className="form" >
+                                <i className="iconfont icon-seach" />
+                                <span className="text">请输入小区或地址</span>
+                            </div>
+                        </Flex>
+                        {/* 右侧地图图标 */}
+                        <i className="iconfont icon-map" />
+                    </Flex>
+                </Flex>
+            
+                {/* 筛选条件菜单 */}
+                <Filter></Filter>
+            </div>
+
         )
     }
 }
 
-export default HomeFind
+export default Find
