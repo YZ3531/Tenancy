6d7a3f2af29e475f7f9ab45dbed18a2862b54776
YZ3531 <2418998035@qq.com>
2020-01-17 23:42:57 +0800

    找房模块-下拉弹窗数据填充

--- src/views/Find/components/Filter/index.js
+++ src/views/Find/components/Filter/index.js
@@ -6,6 +6,9 @@ import FilterMore from '../FilterMore'
 
 import styles from './index.module.css'
 
+import { getCurrentCity } from '../../../../utils/API'
+
+
 export default class Filter extends Component {
   state = {
     menuStatus: {
@@ -15,7 +18,30 @@ export default class Filter extends Component {
       more: false
     },
     // 当前选中的筛选条件
-    openType: null
+    openType: null,
+    // 弹窗内容信息
+    filtersData: []
+  }
+  // 获取弹窗内容信息
+  loadData = async () => {
+    // 获取城市信息
+    let city = await getCurrentCity()
+
+    // 使用当前城市value值进行查询
+    let res = await this.$axios.get('/houses/condition', {
+      params: {
+        id: city.value
+      }
+    })
+
+    // 设置给本地数据
+    this.setState({
+      filtersData: res.body
+    })
+
+  }
+  componentDidMount () {
+    this.loadData()
   }
   // 修改菜单高亮状态
   changeStatus = (type) => {
@@ -42,19 +68,41 @@ export default class Filter extends Component {
 
   }
   // 控制弹窗的关闭-取消
-  onCancel=()=>{
+  onCancel = () => {
     this.setState({
-      openType:null
+      openType: null
     })
   }
   // 控制弹窗的关闭-确定
-  onSave=()=>{
+  onSave = () => {
     this.setState({
-      openType:null
+      openType: null
     })
   }
   render () {
-    let { openType } = this.state
+    let { openType, filtersData: { area, subway, rentType, price } } = this.state
+
+    // 根据点击的筛选条件组装弹窗的列表数据
+    let data = null // 数据
+    let cols = 1 // 列
+    // 下拉弹窗组件只有一份，但是数据可以动态填充
+    switch (openType) {
+      case 'area':
+        // 区域筛选
+        data = [area, subway]
+        cols = 3
+        break;
+      case 'mode':
+        data = rentType
+        cols = 1
+        break;
+      case 'price':
+        data = price
+        cols = 1
+        break;
+      default:
+        break;
+    }
     return (
       <div className={styles.root}>
         {/* 前三个菜单的遮罩层 */}
@@ -66,7 +114,7 @@ export default class Filter extends Component {
           <FilterTitle changeStatus={this.changeStatus} menuStatus={this.state.menuStatus} />
 
           {/* 前三个菜单对应的内容： */}
-          {openType === 'area' || openType === 'mode' || openType === 'price' ? <FilterPicker onSave={this.onSave} onCancel={this.onCancel} /> : ''}
+          {openType === 'area' || openType === 'mode' || openType === 'price' ? <FilterPicker data={data} cols={cols} onSave={this.onSave} onCancel={this.onCancel} /> : ''}
 
           {/* 最后一个菜单对应的内容： */}
           {/* <FilterMore /> */}
